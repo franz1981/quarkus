b7d11d1beba4f9ac1bd2afb5a4381699ba052658
Francesco Nigro <nigro.fra@gmail.com>
2023-07-28 16:53:12 +0200

    Improvements on VertxUtil(s)

--- extensions/resteasy-classic/resteasy/runtime/src/main/java/io/quarkus/resteasy/runtime/standalone/VertxUtil.java
+++ extensions/resteasy-classic/resteasy/runtime/src/main/java/io/quarkus/resteasy/runtime/standalone/VertxUtil.java
@@ -45,10 +45,11 @@ public class VertxUtil {
         }
 
         // ResteasyUriInfo expects a context path to start with a "/" character
-        if (!contextPath.startsWith("/")) {
+        if (contextPath.isEmpty()) {
+            contextPath = "/";
+        } else if (contextPath.charAt(0) != '/') {
             contextPath = "/" + contextPath;
         }
-
         return new ResteasyUriInfo(uriString, contextPath);
     }
 
@@ -105,7 +106,6 @@ public class VertxUtil {
 
     public static MultivaluedMap<String, String> extractRequestHeaders(HttpServerRequest request) {
         Headers<String> requestHeaders = new Headers<String>();
-
         for (Map.Entry<String, String> header : request.headers()) {
             requestHeaders.add(header.getKey(), header.getValue());
         }
